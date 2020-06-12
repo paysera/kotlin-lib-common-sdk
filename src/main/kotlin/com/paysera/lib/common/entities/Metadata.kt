5a9120a7cc7f9bbd3d7e275445071471b3bfb94f
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-06-12 11:29:58 +0300

    Fix Metadata.kt

--- src/main/kotlin/com/paysera/lib/common/entities/Metadata.kt
+++ src/main/kotlin/com/paysera/lib/common/entities/Metadata.kt
@@ -9,7 +9,7 @@ data class Metadata(
     val limit: Int = 0,
     val offset: Int = 0,
     @Json(name = "has_next")
-    val hasNext: Boolean = false,
+    val hasNext: Boolean? = false,
     @Json(name = "has_previous")
-    val hasPrevious: Boolean = false
+    val hasPrevious: Boolean? = false
 )
