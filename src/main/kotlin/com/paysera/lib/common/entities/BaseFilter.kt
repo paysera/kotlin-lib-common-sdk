1077622ccff55f756ff4d96f1dee55e84a5e4e42
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-06-18 10:38:18 +0300

    Don't create moshi adapter for base filter

--- src/main/kotlin/com/paysera/lib/common/entities/BaseFilter.kt
+++ src/main/kotlin/com/paysera/lib/common/entities/BaseFilter.kt
@@ -1,15 +1,9 @@
 package com.paysera.lib.common.entities
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
 open class BaseFilter(
     var offset: Int? = null,
     var limit: Int? = null,
-    @Json(name = "order_by")
     var orderBy: String? = null,
-    @Json(name = "order_direction")
     var orderDirection: String? = null,
     var after: String? = null,
     var before: String? = null
